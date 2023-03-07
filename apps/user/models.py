6256e09dc8274f0b959a8c75158a147ec1e860f7
Santiago Corrales <santi368444110@gmail.com>
2023-03-07 09:40:51 -0500

    refactor: change user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,13 @@
+# Import django
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Model to store users
+    """
+    email = models.EmailField(unique=True)
+    
+    def __str__(self) -> str:
+        return self.username

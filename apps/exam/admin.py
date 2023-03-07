4d86e111926c0216e323b8ddda2af9cc2625e675
Santiago Corrales <santi368444110@gmail.com>
2023-03-07 11:45:56 -0500

    feat: add model exam, register django admin

--- apps/exam/admin.py
+++ apps/exam/admin.py
@@ -1,3 +1,10 @@
+# Import django
 from django.contrib import admin
 
-# Register your models here.
+# From self app
+from . import models
+
+admin.site.register(models.Exam)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
+admin.site.register(models.AnswerStudent)
--- apps/exam/models.py
+++ apps/exam/models.py
@@ -1,3 +1,63 @@
+# Import django
 from django.db import models
 
-# Create your models here.
+# Import shared app
+from apps.shared.constants import ConstantsCharFields
+
+# Import project apps
+from apps.student.models import Student
+
+
+class Exam(models.Model):
+    """
+    Model to store exams
+    """
+    name = models.CharField(max_length=ConstantsCharFields.MAX_LENGTH_MEDIUM)
+    description = models.CharField(max_length=ConstantsCharFields.MAX_LENGTH_EXTRA_LARGE)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Question(models.Model):
+    """
+    Model to store questions
+    """
+    text = models.CharField(max_length=ConstantsCharFields.MAX_LENGTH_LARGE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.text
+    
+
+class Answer(models.Model):
+    """
+    Model to store answer
+    """
+    text = models.CharField(max_length=ConstantsCharFields.MAX_LENGTH_MEDIUM)
+    is_correct = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.text} {self.question} - {self.is_correct}'
+    
+
+class AnswerStudent(models.Model):
+    """
+    Model to store answers of students
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    is_correct = models.BooleanField()
+    
+    def save(self, *args, **kwargs):
+        self.is_correct = self.answer.is_correct
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f'{self.question} {self.is_correct}'
+    
+
+
+    

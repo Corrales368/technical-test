827f218b478c40facabcb2d2fe3ca5671581e08b
Santiago Corrales <santi368444110@gmail.com>
2023-03-08 14:47:41 -0500

    feat: filter records by owned by each student

--- apps/exam/admin.py
+++ apps/exam/admin.py
@@ -4,7 +4,22 @@ from django.contrib import admin
 # From self app
 from . import models
 
+
+class AnswerStudentModelAdmin(admin.ModelAdmin):
+    """
+    Custom class to override want and filter records by owned by each student
+    """
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        new_queryset = queryset.filter(student=request.user.student)
+        return new_queryset
+    
+
 admin.site.register(models.Exam)
 admin.site.register(models.Question)
 admin.site.register(models.Answer)
-admin.site.register(models.AnswerStudent)
+admin.site.register(models.AnswerStudent, AnswerStudentModelAdmin)
+
+
+
+    

85c23478059f6f25857e9f215f3f6395973eb75e
Santiago Corrales <santi368444110@gmail.com>
2023-03-09 10:14:42 -0500

    refactor: change database connection production

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -11,7 +11,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'dbprod.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': os.environ['POSTGRES_PORT'],
     }
 }
